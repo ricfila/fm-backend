e0bd32944e120acead0024a9f53b1ec6ef0cfa52
ByteCntrl <bytecntrl@gmail.com>
2023-08-13 15:48:29 +0200

    Pre-commit

--- backend/routers/_manage.py
+++ backend/routers/_manage.py
@@ -7,7 +7,9 @@ from fastapi import APIRouter, FastAPI
 def load_routers(app: FastAPI):
     path = pathlib.Path(__file__).parent
     package = ".".join(path.relative_to(pathlib.Path(".").absolute()).parts)
-    files = list(filter(lambda x: not x.name.startswith("_"), path.glob("*.py")))
+    files = list(
+        filter(lambda x: not x.name.startswith("_"), path.glob("*.py"))
+    )
 
     for x in files:
         module = importlib.import_module(f"{package}.{x.stem}")
