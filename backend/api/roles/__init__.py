45e58454564483a232075eca51b1673477b552dd
ByteCntrl <bytecntrl@gmail.com>
2024-05-01 21:19:49 +0200

    Delete get_roles_name route

--- backend/api/roles/__init__.py
+++ backend/api/roles/__init__.py
@@ -4,7 +4,6 @@ __all__ = (
     "delete_role_router",
     "get_role_router",
     "get_roles_router",
-    "get_roles_name_router",
     "update_role_permissions_router",
     "update_role_name_router",
     "update_role_paper_size_router",
@@ -16,7 +15,6 @@ from .create_role import create_role_router
 from .delete_role import delete_role_router
 from .get_role import get_role_router
 from .get_roles import get_roles_router
-from .get_roles_name import get_roles_name_router
 from .udpate_role_permissions import update_role_permissions_router
 from .update_role_name import update_role_name_router
 from .update_role_paper_size import update_role_paper_size_router
@@ -24,7 +22,6 @@ from .update_role_paper_size import update_role_paper_size_router
 roles = APIRouter(prefix="/roles", tags=["roles"])
 roles.include_router(create_role_router)
 roles.include_router(delete_role_router)
-roles.include_router(get_roles_name_router)
 roles.include_router(get_role_router)
 roles.include_router(get_roles_router)
 roles.include_router(update_role_permissions_router)
