82e0c95e0ededfcffba4861f9e4faf94343b6170
Francesco Contran <bytecntrl@gmail.com>
2025-03-03 11:34:56 +0100

    Update get_subcategories endpoint

--- backend/api/subcategories/get_subcategories.py
+++ backend/api/subcategories/get_subcategories.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends
-from tortoise.exceptions import FieldError
+from tortoise.exceptions import ParamsError
+from tortoise.expressions import Q
 from tortoise.transactions import in_transaction
 
-from backend.config import Session
 from backend.database.models import Subcategory
-from backend.models.error import NotFound
+from backend.models.error import BadRequest
 from backend.models.subcategories import (
     GetSubcategoriesResponse,
     Subcategory as SubcategoryModel,
     SubcategoryName,
 )
 from backend.utils import ErrorCodes, TokenJwt, validate_token
+from backend.utils.query_utils import process_query_with_pagination
 
 get_subcategories_router = APIRouter()
 
@@ -30,33 +31,27 @@ async def get_subcategories(
     """
 
     async with in_transaction() as connection:
-        subcategories_query = Subcategory.all().using_db(connection)
+        (
+            subcategories_query,
+            total_count,
+            limit,
+        ) = process_query_with_pagination(
+            Subcategory, Q(), connection, offset, limit, order_by
+        )
 
-        if order_by:
-            try:
-                subcategories_query = subcategories_query.order_by(order_by)
-            except FieldError:
-                raise NotFound(code=ErrorCodes.UNKNOWN_ORDER_BY_PARAMETER)
-
-        total_count = await subcategories_query.count()
-
-        if not limit:
-            limit = (
-                total_count - offset
-                if only_name
-                else Session.config.DEFAULT_LIMIT_VALUE
+        try:
+            subcategories = await subcategories_query.offset(offset).limit(
+                limit
             )
+        except ParamsError:
+            raise BadRequest(code=ErrorCodes.INVALID_OFFSET_OR_LIMIT_NEGATIVE)
 
-        subcategories = await subcategories_query.offset(offset).limit(limit)
-
-        subcategories_list = [
+    return GetSubcategoriesResponse(
+        total_count=total_count,
+        subcategories=[
             SubcategoryName(**await subcategory.to_dict_name())
             if only_name
             else SubcategoryModel(**await subcategory.to_dict())
             for subcategory in subcategories
-        ]
-
-    return GetSubcategoriesResponse(
-        total_count=total_count,
-        subcategories=subcategories_list,
+        ],
     )
